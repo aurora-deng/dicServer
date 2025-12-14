4ed4770ce8b64e287676b146e70d6b3c836a9db0
pikachu <13979657815@163.com>
2025-12-14 22:29:32 +0800

    完成该文件功能编写

--- database_manager.cpp
+++ database_manager.cpp
@@ -1,11 +1,10 @@
-#include"database_manager.hpp"
-#include<fstream>                           //IO操作的头文件
-#include<ctime>                             //时间函数
-#include<stdexcept>                         //异常处理头文件
-#define  DICT_PATH "./dict.txt"        
+#include "database_manager.hpp"
+#include <fstream>   //IO操作的头文件
+#include <ctime>     //时间函数
+#include <stdexcept> //异常处理头文件
+#define DICT_PATH "./dict.txt"
 
 using namespace std;
-
 
 /*
 构造函数：打开数据库连接
@@ -14,25 +13,24 @@ using namespace std;
 返回值：如果数据库打开失败，则返回异常处理：runtime_error
 */
 
-
-DatabaseManger::DatabaseManger(const string &user_db_path,const string &dic_db_path)
+DatabaseManger::DatabaseManger(const string &user_db_path, const string &dic_db_path)
 {
     // 打开或创建用户数据库
-    if(sqlite3_open(user_db_path.c_str(),&usr_db_)!=SQLITE_OK)
+    if (sqlite3_open(user_db_path.c_str(), &usr_db_) != SQLITE_OK)
     {
-        throw runtime_error("用户数据打开失败："+string(sqlite3_errmsg(usr_db_)));
+        throw runtime_error("用户数据打开失败：" + string(sqlite3_errmsg(usr_db_)));
     }
     // 打开或者创建数据库
-    if(sqlite3_open(dic_db_path.c_str(),&dict_db_)!=SQLITE_OK)
+    if (sqlite3_open(dic_db_path.c_str(), &dict_db_) != SQLITE_OK)
     {
-        throw runtime_error("单词库数据打开失败："+string(sqlite3_errmsg(dict_db_)));
+        throw runtime_error("单词库数据打开失败：" + string(sqlite3_errmsg(dict_db_)));
     }
 }
 
 DatabaseManger::~DatabaseManger()
 {
-    sqlite3_close(usr_db_);          //关闭用户数据库
-    sqlite3_close(dict_db_);          //关闭单词数据库
+    sqlite3_close(usr_db_);  // 关闭用户数据库
+    sqlite3_close(dict_db_); // 关闭单词数据库
 }
 
 /*
@@ -41,8 +39,8 @@ DatabaseManger::~DatabaseManger()
 
 bool DatabaseManger::initalizeDatabase()
 {
-    //用户表和单词表都成功后，表示数据库初始化成功
-    return initalizeUserDB()&&initalizeDictDB();
+    // 用户表和单词表都成功后，表示数据库初始化成功
+    return initalizeUserDB() && initalizeDictDB();
 }
 
 /*
@@ -55,34 +53,33 @@ bool DatabaseManger::initalizeUserDB()
     lock_guard<mutex> lock(usr_mutex_);
 
     // 准备sql语句：创建用户表和历史记录sql语句
-    const char *sql="create table if not exists usr("       //创建用户表
-                        "name text primary key,"            //用户名（主键）
-                        "password int,"                     //密码
-                        "stage int);"                       //用户状态：0表示离线，1表示在线    
-                        "create table if not exists history("   //创建历史记录表
-                        "name text,"                            //用户名
-                        "word text,"                            //查询单词
-                        "mean text,"                            //单词意思
-                        "time text);"  ;                         //查询时间
-    
+    const char *sql = "create table if not exists usr("     // 创建用户表
+                      "name text primary key,"              // 用户名（主键）
+                      "password int,"                       // 密码
+                      "stage int);"                         // 用户状态：0表示离线，1表示在线
+                      "create table if not exists history(" // 创建历史记录表
+                      "name text,"                          // 用户名
+                      "word text,"                          // 查询单词
+                      "mean text,"                          // 单词意思
+                      "time text);";                        // 查询时间
+
     // 执行sql语句
-    if(!executeSQL(usr_db_,sql))
+    if (!executeSQL(usr_db_, sql))
     {
-        cout<<"数据表初始化失败"<<endl;
+        cout << "数据表初始化失败" << endl;
         return false;
     }
 
     // 重置所有的客户都为离线状态
-    sql="update usr set stage=0;";
-    if(!executeSQL(usr_db_,sql))
+    sql = "update usr set stage=0;";
+    if (!executeSQL(usr_db_, sql))
     {
-        cout<<"用户状态重置失败"<<endl;
+        cout << "用户状态重置失败" << endl;
         return false;
     }
 
-    return true;         //表示两个数据表创建成功，并且将所有客户端设置成离线状态
+    return true; // 表示两个数据表创建成功，并且将所有客户端设置成离线状态
 }
-
 
 bool DatabaseManger::initalizeDictDB()
 {
@@ -90,16 +87,16 @@ bool DatabaseManger::initalizeDictDB()
     lock_guard<mutex> lock(dict_mutex_);
 
     // 准备创建单词数据库的sql语句
-    const char *sql="create table if not exists dict(word text,mean text);";
-    if(!executeSQL(dict_db_,sql))
+    const char *sql = "create table if not exists dict(word text,mean text);";
+    if (!executeSQL(dict_db_, sql))
     {
-        cerr<<"单词表创建失败"<<endl;
+        cerr << "单词表创建失败" << endl;
         return false;
     }
 
     // 检查词典数据库是否为空
-    sql="select * from dict;";              //查询数据库中的记录个数
-    sqlite3_stmt *stmt;                     //接受预处理sql的结果
+    sql = "select * from dict;"; // 查询数据库中的记录个数
+    sqlite3_stmt *stmt;          // 接受预处理sql的结果
 
     /*
     将上面的sql预编译一下
@@ -110,23 +107,21 @@ bool DatabaseManger::initalizeDictDB()
     参数4：编译后的结果存放的位置
     参数5：接收没有处理的剩余的sql语句
     */
-    if(sqlite3_prepare_v2(dict_db_,sql,-1,&stmt,NULL)!=SQLITE_OK)
+    if (sqlite3_prepare_v2(dict_db_, sql, -1, &stmt, NULL) != SQLITE_OK)
     {
-        cerr<<"sql准备失败"<<sqlite3_errmsg(dict_db_)<<endl;
+        cerr << "sql准备失败" << sqlite3_errmsg(dict_db_) << endl;
         return false;
     }
 
-
-    bool need_import=false;                     //标识是否需要导入数据
-
+    bool need_import = false; // 标识是否需要导入数据
 
     // 执行sql语句
-    if(sqlite3_step(stmt)==SQLITE_OK)       //表示执行成功
+    if (sqlite3_step(stmt) == SQLITE_OK) // 表示执行成功
     {
-        if(sqlite3_column_int(stmt,0)==0)   //表示数据为空
+        if (sqlite3_column_int(stmt, 0) == 0) // 表示数据为空
         {
             // 表示表为空，需要导入数据
-            need_import=true;
+            need_import = true;
         }
     }
 
@@ -135,7 +130,7 @@ bool DatabaseManger::initalizeDictDB()
     sqlite3_finalize(stmt);
 
     // 如果需要导入，返回导入函数结果，如果已有数据表结果，则直接返回
-    return need_import?dictToDatabase():true;
+    return need_import ? dictToDatabase() : true;
 }
 
 /*
@@ -146,65 +141,64 @@ bool DatabaseManger::dictToDatabase()
 {
     // 使用文件操作，打开文件dict.txt
     ifstream file(DICT_PATH);
-    if(!file)
+    if (!file)
     {
-        cerr<<"无法打开词典文件:"<<DICT_PATH<<endl;
+        cerr << "无法打开词典文件:" << DICT_PATH << endl;
         return false;
     }
 
-    string line ;                   //用于读取文件中的一行数据
-    while(getline(file,line))       //循环读取file中的一行信息放入到line中
+    string line;                // 用于读取文件中的一行数据
+    while (getline(file, line)) // 循环读取file中的一行信息放入到line中
     {
         // 跳过空行
-        if(line.empty())continue;
+        if (line.empty())
+            continue;
 
         // 分割单词和意义 格式（单词 意义）
-        size_t pos=line.find(' ');              //定位到空格的位置
-        if(pos==string::npos)
+        size_t pos = line.find(' '); // 定位到空格的位置
+        if (pos == string::npos)
         {
-            cerr<<"无效词典条目（确实空格分隔符）"<<line<<endl;
+            cerr << "无效词典条目（确实空格分隔符）" << line << endl;
             continue;
         }
 
         // 将单词和意义分开储存
-        string word=line.substr(0,pos);
-        string mean=line.substr(pos+1);
+        string word = line.substr(0, pos);
+        string mean = line.substr(pos + 1);
 
         // 准备sql语句可以使用通配符表示
-        const char* sql="insert into dict values(?,?)";
-        
-        // 预编译sql语句，
-        sqlite3_stmt *stmt=NULL;
+        const char *sql = "insert into dict values(?,?)";
 
-        if(sqlite3_prepare_v2(dict_db_,sql,-1,&stmt,NULL)!=SQLITE_OK)
+        // 预编译sql语句，
+        sqlite3_stmt *stmt = NULL;
+
+        if (sqlite3_prepare_v2(dict_db_, sql, -1, &stmt, NULL) != SQLITE_OK)
         {
-            cerr<<"sql准备失败"<<sqlite3_errmsg(dict_db_)<<endl;
+            cerr << "sql准备失败" << sqlite3_errmsg(dict_db_) << endl;
             file.close();
             return false;
         }
 
         // 预编译成功，可以为已经编译成功的内容绑定参数
-        sqlite3_bind_text(stmt,1,word.c_str(),-1,SQLITE_STATIC);
-        sqlite3_bind_text(stmt,2,mean.c_str(),-1,SQLITE_STATIC);
-
+        sqlite3_bind_text(stmt, 1, word.c_str(), -1, SQLITE_STATIC);
+        sqlite3_bind_text(stmt, 2, mean.c_str(), -1, SQLITE_STATIC);
 
         // 执行插入语句
-        if(sqlite3_step(stmt)!=SQLITE_DONE)
+        if (sqlite3_step(stmt) != SQLITE_DONE)
         {
-            cerr<<"数据插入失败："<<sqlite3_errmsg(dict_db_)<<endl;
+            cerr << "数据插入失败：" << sqlite3_errmsg(dict_db_) << endl;
             sqlite3_finalize(stmt);
             file.close();
             return false;
         }
 
         // 程序执行到此，表示该单词已经导入到数据库中
-            sqlite3_finalize(stmt);
-
+        sqlite3_finalize(stmt);
     }
 
-     //程序执行至此，表示，单词表导入成功
+    // 程序执行至此，表示，单词表导入成功
     file.close();
-    cout<<"单词库导入成功"<<endl;
+    cout << "单词库导入成功" << endl;
     return true;
 }
 
@@ -213,141 +207,260 @@ bool DatabaseManger::dictToDatabase()
 参数：数据库语柄，要执行的sql语句，错误信息
 */
 
-bool DatabaseManger::executeSQL(sqlite3 *ppdb,const string &sql,char **errmsg)
+bool DatabaseManger::executeSQL(sqlite3 *ppdb, const string &sql, char **errmsg)
 {
-    if(sqlite3_exec(ppdb,sql.c_str(),NULL,NULL,errmsg)!=SQLITE_OK)
+    if (sqlite3_exec(ppdb, sql.c_str(), NULL, NULL, errmsg) != SQLITE_OK)
     {
-        cerr<<"sql执行失败："<<*errmsg<<endl;
+        cerr << "sql执行失败：" << *errmsg << endl;
         // 释放错误信息空间
         sqlite3_free(errmsg);
         return false;
     }
 
-    return true;                //表示正常执行了sql语句
+    return true; // 表示正常执行了sql语句
 }
-
 
 /*
 注册功能；就是将用户名和密码放入到usr表中，如果有重复就报错
 参数：用户名，密码
 */
 
-bool DatabaseManger::registerUser(const string &name,const string &password)
+bool DatabaseManger::registerUser(const string &name, const string &password)
 {
     // 使用互斥锁防止多线程出现竞态
     lock_guard<mutex> lock(usr_mutex_);
 
     // 准备sql语句，使用通配符来描述，后面直接绑定即可，全部都初始化为0的离线状态
-    const char* sql="insert into usr values(?,?,0);";
+    const char *sql = "insert into usr values(?,?,0);";
 
-    sqlite3_stmt *stmt=NULL;
-    if(sqlite3_prepare_v2(usr_db_,sql,  -1,&stmt,NULL)!=SQLITE_OK)
+    sqlite3_stmt *stmt = NULL;
+    if (sqlite3_prepare_v2(usr_db_, sql, -1, &stmt, NULL) != SQLITE_OK)
     {
-        cerr<<"sql语句执行失败"<<sqlite3_errmsg(usr_db_)<<endl;
+        cerr << "sql语句执行失败" << sqlite3_errmsg(usr_db_) << endl;
         return false;
     }
 
     // 预编译成功之后就可以给通配符进行bind
-    sqlite3_bind_text(stmt,1,name.c_str(),-1,SQLITE_STATIC);
-    sqlite3_bind_text(stmt,2,password.c_str(),-1,SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 1, name.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, password.c_str(), -1, SQLITE_STATIC);
 
     // 执行插入操作
-    int result=sqlite3_step(stmt);
+    int result = sqlite3_step(stmt);
     sqlite3_finalize(stmt);
 
-
     // 对于执行结果进行判断
-    if(result==SQLITE_CONSTRAINT)
+    if (result == SQLITE_CONSTRAINT)
     {
         // 违反主键唯一性
-        cerr<<"该用户名已存在；"<<name<<endl;
+        cerr << "该用户名已存在；" << name << endl;
         return false;
     }
 
     // 表示注册成功
-    return result=SQLITE_DONE;
-
+    return result = SQLITE_DONE;
 }
-
-
 
 /*
 用户登入功能：
 参数：用户名，密码，判断是否在线，防止冲突
 */
 
-
-bool DatabaseManger::loginUser(const string &name,const string &password,bool is_online)
+bool DatabaseManger::loginUser(const string &name, const string &password, bool is_online)
 {
     // 同样加锁
     lock_guard<mutex> lock(usr_mutex_);
 
     // 准备sql语句
-    const char* sql="select stage from usr where name=? and passeord=?;";
+    const char *sql = "select stage from usr where name=? and passeord=?;";
 
     // 预编译测试
-    sqlite3_stmt *stmt=NULL;
-    if(sqlite3_prepare_v2(usr_db_,sql,-1,&stmt,NULL)!=SQLITE_OK)
+    sqlite3_stmt *stmt = NULL;
+    if (sqlite3_prepare_v2(usr_db_, sql, -1, &stmt, NULL) != SQLITE_OK)
     {
-        cerr<<"sql准备失败："<<sqlite3_errmsg(usr_db_)<<endl;
+        cerr << "sql准备失败：" << sqlite3_errmsg(usr_db_) << endl;
         return false;
     }
 
-
     // 预编译成功，可以为通配符绑定上要真正输入的
-    sqlite3_bind_text(stmt,1,name.c_str(),-1,SQLITE_STATIC);
-    sqlite3_bind_text(stmt,2,password.c_str(),-1,SQLITE_STATIC);
-
+    sqlite3_bind_text(stmt, 1, name.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, password.c_str(), -1, SQLITE_STATIC);
 
     // 执行编译操作
-    int result=sqlite3_step(stmt);
+    int result = sqlite3_step(stmt);
 
     // 对于执行结果进行判断
-    if(result==SQLITE_ROW)
+    if (result == SQLITE_ROW)
     {
         // 获取用户的状态
-        if(!is_online)
+        if (!is_online)
         {
-            sqlite3_finalize(stmt);     //释放空间
+            sqlite3_finalize(stmt); // 释放空间
 
             // 准备sql语句，进行预编译,更新该用户的状态
-            const char *update_sql="update usr set stage=1 where name=?;";
+            const char *update_sql = "update usr set stage=1 where name=?;";
 
-            if(sqlite3_prepare_v2(usr_db_,update_sql,-1,&stmt,NULL)!=SQLITE_OK)
+            if (sqlite3_prepare_v2(usr_db_, update_sql, -1, &stmt, NULL) != SQLITE_OK)
             {
-                cerr<<"sql准备失败"<<sqlite3_errmsg(usr_db_)<<endl;
+                cerr << "sql准备失败" << sqlite3_errmsg(usr_db_) << endl;
                 return false;
             }
 
             // 如果预编译成功，就执行绑定操作
 
-            sqlite3_bind_text(stmt,1,name.c_str(),1,SQLITE_STATIC);
+            sqlite3_bind_text(stmt, 1, name.c_str(), 1, SQLITE_STATIC);
             // 更新运行结果
-            result=sqlite3_step(stmt);
-
+            result = sqlite3_step(stmt);
         }
     }
-    
+
     // 释放空间
     sqlite3_finalize(stmt);
-    return result=SQLITE_DONE;
-
+    return result = SQLITE_DONE;
 }
 
+/*
+    用户退出功能：将该用户对对应的状态调整成0即可
+    参数：用户名
+*/
+
+bool DatabaseManger::logoutUser(const string &name)
+{
+    // 依旧上锁
+    lock_guard<mutex> lock(usr_mutex_);
+
+    // 准备sql语句
+    const char *sql = "update usr set stage=0 where name=?;";
+
+    // 预编译sql语句
+    sqlite3_stmt *stmt = NULL;
+
+    if (sqlite3_prepare_v2(usr_db_, sql, -1, &stmt, NULL) != SQLITE_OK)
+    {
+        cout << "sql语句准备失败" << sqlite3_errmsg(usr_db_) << endl;
+        return false;
+    }
+
+    // 绑定信息
+    int result = sqlite3_bind_text(stmt, 1, name.c_str(), -1, SQLITE_STATIC);
+
+    // 执行更新操作
+    sqlite3_step(stmt);
+    return result == SQLITE_DONE;
+}
 
 /*
 
 查单词功能：
     参数：单词，返回意义
 */
-bool DatabaseManger::querryWord(const string &word,string &meaning)
+bool DatabaseManger::querryWord(const string &word, string &meaning)
 {
     // 老样子，多线程上锁
     lock_guard<mutex> lock(dict_mutex_);
 
     // 准备sql语句
-    const char * sql="select mean from dict where word=?;";
+    const char *sql = "select mean from dict where word=?;";
     // 预编译sql语句
-    sqlite3_stmt *stmt=NULL;
-    if(sqlite3_prepare_v2(dict_))
+    sqlite3_stmt *stmt = NULL;
+    if (sqlite3_prepare_v2(dict_db_, sql, -1, &stmt, NULL) != SQLITE_OK)
+    {
+        cerr << "sql准备失败" << sqlite3_errmsg(dict_db_) << endl;
+        return false;
+    }
+
+    // 绑定信息
+    sqlite3_bind_text(stmt, 1, word.c_str(), -1, SQLITE_STATIC);
+
+    // 执行查询结果
+    bool found = false; // 标识是否查询到
+    if (sqlite3_step(stmt) == SQLITE_ROW)
+    {
+        // 获取查询结果
+        const unsigned char *result = sqlite3_column_text(stmt, 0);
+        if (result)
+        {
+            meaning = reinterpret_cast<const char *>(result); // 转换数据类型
+            found = true;
+        }
+    }
+
+    // 释放空间
+    sqlite3_finalize(stmt);
+    return found;
+}
+
+/*
+    历史记录函数：将给定的信息插入到历史数据库中
+    参数1：用户名
+    参数2：单词
+    参数3：含义
+    参数4：查询时间
+*/
+
+bool DatabaseManger::recordHistory(const string &name, const string &word, const string &meaning, const string &time)
+{
+    // 依旧使用互斥锁防止竞态
+    lock_guard<mutex> lock(usr_mutex_);
+
+    // 准备预编译sql语句
+    const char *sql = "insert into history values(?,?,?,?);";
+    sqlite3_stmt *stmt = NULL;
+    if (sqlite3_prepare_v2(usr_db_, sql, -1, &stmt, NULL) != SQLITE_OK)
+    {
+        cerr << "sql预编译失败" << sqlite3_errmsg(usr_db_) << endl;
+        return false;
+    }
+
+    // 绑定信息
+    sqlite3_bind_text(stmt, 1, name.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 1, word.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 1, meaning.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 1, time.c_str(), -1, SQLITE_STATIC);
+
+    // 执行插入操作
+    int result = sqlite3_step(stmt);
+    sqlite3_finalize(stmt);
+    return result = SQLITE_DONE;
+}
+
+/*
+    获取查单词的历史记录
+    参数1：用户名
+    参数2：查询的结果字符串
+*/
+bool DatabaseManger::getHistory(const string &name, string &History)
+{
+    lock_guard<mutex> lock(dict_mutex_);
+
+    const char *sql = "select word,meaning,time from history where name=?; order by time DESC";
+
+    // 预编译sql
+    sqlite3_stmt *stmt = NULL;
+    if (sqlite3_prepare_v2(usr_db_, sql, -1, &stmt, NULL) != SQLITE_OK)
+    {
+        cerr << "sql预编译错误" << sqlite3_errmsg(usr_db_) << endl;
+        return false;
+    }
+
+    sqlite3_bind_text(stmt, 1, name.c_str(), -1, SQLITE_STATIC);
+
+    // 执行查询语句
+    History.clear(); // 防止之前的数据干扰
+
+    while (sqlite3_step(stmt) == SQLITE_OK)
+    {
+        // 获得该信息的每一列
+        const unsigned char *word = sqlite3_column_text(stmt, 0);
+        const unsigned char *mean = sqlite3_column_text(stmt, 1);
+        const unsigned char *time = sqlite3_column_text(stmt, 2);
+        // 将数据变成一条字符串
+        if (word && mean && time)
+        {
+            History+=string(reinterpret_cast<const char *>(word))+"\t";
+            History+=string(reinterpret_cast<const char *>(mean))+"\t";
+            History+=string(reinterpret_cast<const char *>(time))+"\t";
+        }
+    }
+    sqlite3_finalize(stmt);
+    return true;
 }
